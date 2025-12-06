ca8ced361db8414f8d74415eaee1606ce639d5da
void-pronway <pronwaymitra135@gmail.com>
2025-12-06 23:09:53 +0600

    initial

---
+++ Hotel-Booking-System/src/test/java/com/hbs/Hotel/Booking/System/HotelBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbs.Hotel.Booking.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ HotelWeb/src/app/app.config.ts
@@ -0,0 +1,19 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { en_US, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+
+registerLocaleData(en);
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideNzI18n(en_US), provideAnimationsAsync(), provideHttpClient()
+  ]
+};
